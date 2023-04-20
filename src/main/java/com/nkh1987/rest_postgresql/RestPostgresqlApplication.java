1c3e069e0b14cf882e2acb7cff29d14443f15b9c
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-20 15:02:45 +0330

    Update POM

---
+++ src/main/java/com/nkh1987/rest_postgresql/RestPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.nkh1987.rest_postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPostgresqlApplication.class, args);
+	}
+
+}
