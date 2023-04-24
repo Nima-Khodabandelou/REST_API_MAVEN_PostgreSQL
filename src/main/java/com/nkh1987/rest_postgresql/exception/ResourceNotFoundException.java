7e83a09cdecb100899b2b33641273fc90cde4727
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-24 21:03:53 +0330

    Update ResourceNotFoundException.java

--- src/main/java/com/nkh1987/rest_postgresql/exception/ResourceNotFoundException.java
+++ src/main/java/com/nkh1987/rest_postgresql/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.nkh1987.rest_postgresql.exception;
+opackage com.nkh1987.rest_postgresql.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,19 +8,14 @@ public class ResourceNotFoundException extends RuntimeException {
 
     /** This class extends an exception class, so if serialversionUID not provided compiler will give error. 
      *
-     *  Repeated from https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html for
-     *  clarity:
-     *
-     *  The serialization runtime associates with each serializable class a version number, called a serialVersionUID,
-     *  which is used during deserialization to verify that the sender and receiver of a serialized object have loaded
-     *  classes for that object that are compatible with respect to serialization. If the receiver has loaded a class
-     *  for the object that has a different serialVersionUID than that of the corresponding sender's class, then
-     *  deserialization will result in an InvalidClassException. A serializable class can declare its own
-     *  serialVersionUID explicitly by declaring a field named serialVersionUID that must be static, final,
-     *  and of type long*/
-    private static final long serialVersionUID = 1L;
+     *  More info in https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html for
+     */
+    
+    // Typically a long random number
+    private static final long serialVersionUID = 13297384567962L;
 
     public ResourceNotFoundException(String message) {
+        // Pass message as argument to the supercalss, i.e. RuntimeException
         super(message);
     }
 }
