8896b8590d28561c252f6f909f1f10c336e020c8
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-24 21:06:23 +0330

    Update StudentRepository.java

--- src/main/java/com/nkh1987/rest_postgresql/repository/StudentRepository.java
+++ src/main/java/com/nkh1987/rest_postgresql/repository/StudentRepository.java
@@ -3,5 +3,7 @@ package com.nkh1987.rest_postgresql.repository;
 import com.nkh1987.rest_postgresql.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// To benefit from Spring Data JPA, the interface should inherits
+// functionalities from JpaRepository 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
