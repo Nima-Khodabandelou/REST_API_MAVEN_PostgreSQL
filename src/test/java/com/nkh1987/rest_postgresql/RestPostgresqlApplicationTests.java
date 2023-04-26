fef2a3ee346d9c6663fb685d7cbaf89f0552a755
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-04-26 07:24:34 +0330

    Update RestPostgresqlApplicationTests.java

--- src/test/java/com/nkh1987/rest_postgresql/RestPostgresqlApplicationTests.java
+++ src/test/java/com/nkh1987/rest_postgresql/RestPostgresqlApplicationTests.java
@@ -1,13 +1,12 @@
 package com.nkh1987.rest_postgresql;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestPostgresqlApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
