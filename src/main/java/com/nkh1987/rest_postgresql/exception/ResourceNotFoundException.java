b12f9adc618c7d785be53ba3672d1195bd1ed699
Nima Khodabandelou <nima.khodabandelou@gmail.com>
2023-05-05 10:52:39 +0330

    Update ResourceNotFoundException.java

--- src/main/java/com/nkh1987/rest_postgresql/exception/ResourceNotFoundException.java
+++ src/main/java/com/nkh1987/rest_postgresql/exception/ResourceNotFoundException.java
@@ -1,15 +1,14 @@
 opackage com.nkh1987.rest_postgresql.exception;
 
 import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-
     /** This class extends an exception class, so if serialversionUID not provided compiler will give error. 
      *  More info in https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/io/Serializable.html for
      */
-    
     // Typically a long random number
     private static final long serialVersionUID = 13297384567962L;
 
